fff5b0c9198e31658f8f324b1ed1cf914f02b21e
wcarthur <craig.arthur@ga.gov.au>
2017-09-04 21:16:46 +1000

    Syntax error in output format statement

--- tcrmextremes.py
+++ tcrmextremes.py
@@ -123,7 +123,7 @@ def main(configFile):
                    " gpd_rate, gpd_shape, gpd_thresh, gpd_scale\n")
     paramfmt = "{}, {}, " + ", ".join(["{:.5f}"] * 8) + "\n"
     rvalheader = "locId, locName, " + ", ".join(["{:d}"]*len(rp)).format(*rp) + "\n"
-    rvalfmt = "{}, {}" + ", ".join(["{:.5f}"] * len(rp)) + "\n"
+    rvalfmt = "{}, {}, " + ", ".join(["{:.5f}"] * len(rp)) + "\n"
     # On the head node:
     if (pp.rank() == 0) and (pp.size() > 1):
         fh = open(pjoin(processPath, "parameters.csv"), "w")
